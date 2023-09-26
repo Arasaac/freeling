4edd98a90a0f88d02f033783fb478d3cdc0c3ed3
juanda <juandacorreo@gmail.com>
2023-09-26 14:06:27 +0200

    fix tab error

--- app/app.py
+++ app/app.py
@@ -102,7 +102,7 @@ def Analizador():
     
 
 # inicilizamos freeling
-	DATA = "/usr/local"+"/share/freeling/"	
+    DATA = "/usr/local"+"/share/freeling/"	
 #    DATA = "/usr"+"/share/freeling/"
 # Init locales
     pyfreeling.util_init_locale("default")
